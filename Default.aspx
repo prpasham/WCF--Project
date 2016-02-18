7af350db338b62218d4ab7dad94948628a7fe203
prpasham <prpasham@my.harrisburgu.edu>
2016-02-17 19:09:39 -0600

    WCF
    WCF

---
+++ Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calculator._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+   
+    <tr>
+    <th><asp:Label ID="lbl1st" runat="server" Text="FirstNumber"></asp:Label></th>
+    <td><asp:TextBox ID="txt1st" runat="server" ></asp:TextBox></td>
+    </tr>
+        <tr>
+    <th ><asp:Label ID="lbl2nd" runat="server" Text="SecondNumber"></asp:Label></th>
+    <td><asp:TextBox ID="txt2nd" runat="server" ></asp:TextBox></td>
+    </tr>
+    <tr>
+    
+    <td ><asp:Button ID="btnAdd" runat="server" Text="+" Width="25" onclick="btnAdd_Click" />  
+    &nbsp;  
+    <asp:Button ID="btnsub" runat="server" Text="-" Width="25" Height="26px" 
+            onclick="btnsub_Click" />
+        &nbsp;<asp:Button ID="btnmul" runat="server" Text="**" Width="25" Height="26px" 
+            onclick="btnmul_Click" />&nbsp;<asp:Button 
+            ID="btndiv" runat="server" Text="/" Width="24px" Height="26px" 
+            onclick="btndiv_Click" />
+  
+        &nbsp; 
+        </td>
+    <td >  
+        <asp:TextBox ID="txtResult" runat="server"></asp:TextBox>
+                    </td>
+    
+    </tr>
+    </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.ServiceModel;
+using Calculator.Myservice;
+
+namespace Calculator
+{
+    public partial class _Default : System.Web.UI.Page
+    { 
+        int a,b;
+        Calculator.Myservice.Service1Client Client= new Calculator.Myservice.Service1Client();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+         Response.Write("MY Calculator");
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(txt1st.Text);
+            b = Convert.ToInt32(txt2nd.Text);
+            int Addition = Client.add(a,b);
+           
+            
+            txtResult.Text = Addition.ToString();
+        }
+
+        protected void btnsub_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(txt1st.Text);
+            b = Convert.ToInt32(txt2nd.Text);
+            int Addition = Client.Sub(a,b);
+            txtResult.Text = Addition.ToString();
+
+        }
+
+        protected void btnmul_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(txt1st.Text);
+            b = Convert.ToInt32(txt2nd.Text);
+            int Addition = Client.Mul(a, b);
+            txtResult.Text = Addition.ToString();
+        }
+
+        protected void btndiv_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(txt1st.Text);
+            b = Convert.ToInt32(txt2nd.Text);
+            int Addition = Client.Div(a, b);
+            txtResult.Text = Addition.ToString();
+
+        }
+       
+    }   
+}
+
